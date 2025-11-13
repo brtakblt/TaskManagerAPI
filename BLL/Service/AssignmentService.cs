df8686efddf2e674292ce26ebfb09572cf3a9e98
berat <berat@MSI>
2025-11-13 12:34:24 +0300

    .

--- BLL/Service/AssignmentService.cs
+++ BLL/Service/AssignmentService.cs
@@ -3,6 +3,7 @@ using ENTITY;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,6 +37,21 @@ namespace BLL.Service
             {
                 await _assignmentDal.DeleteAsync(Id);
             }
+        }
+
+        public async Task<IEnumerable<Assignment>> GetAllAsync(Expression<Func<Assignment, bool>>filter = null)
+        {
+            return await _assignmentDal.GetAllAsync(filter);
+        }
+
+        public async Task<Assignment> GetByIdAsync(int Id)
+        {
+            return await _assignmentDal.GetByIdAsync(Id);
+        }
+
+        public async Task UpdateAsync()
+        {
+            await _assignmentDal.UpdateAsync();
         }
 
 
